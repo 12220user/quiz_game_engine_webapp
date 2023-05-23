080513796d037ecb35f1aabe8b8c4f786ede625e
12220 <supermansec@gmail.com>
2023-05-23 20:05:10 +0300

    1.0.0.3 refactoring code and small tips(dark gradien back question text)

--- Game/TemplateData/game.js
+++ Game/TemplateData/game.js
@@ -7,64 +7,64 @@
 ░░░╚═╝░░░░╚═════╝░╚═╝╚══════╝  ╚══════╝╚═╝░░╚══╝░╚═════╝░╚═╝╚═╝░░╚══╝╚══════╝
 
 by 12220
-Version 1.0.0.2 realise
+Version 1.0.0.3 realise
 actual version https://github.com/12220user/quiz_game_engine_webapp
 documentation - in progress
 */
 
-let addManager = {
+let YaSDKManager = {
     canShowAd: true,
-    review:function(){
+    review: function() {
         ysdk.feedback.canReview()
-        .then(({ value, reason }) => {
-            if (value) {
-                ysdk.feedback.requestReview()
-                    .then(({ feedbackSent }) => {
-                        console.log(feedbackSent);
-                    })
-            } else {
-                console.log(reason)
-            }
-        })
+            .then(({ value, reason }) => {
+                if (value) {
+                    ysdk.feedback.requestReview()
+                        .then(({ feedbackSent }) => {
+                            console.log(feedbackSent);
+                        })
+                } else {
+                    console.log(reason)
+                }
+            })
     },
     showInterstitial: function(callback) {
         MuteAudio(true)
-        if(!this.canShowAd){
+        if (!this.canShowAd) {
             callback()
             MuteAudio(false)
             return
         }
         this.canShowAd = false
         ysdk.adv.showFullscreenAdv({
-            callbacks:{
-                onClose: function(w){callback();MuteAudio(false)}
+            callbacks: {
+                onClose: function(w) {
+                    callback()
+                    MuteAudio(false)
+                }
             },
-            onError:function(e){callback()}
+            onError: function(e) { callback() }
         })
         setTimeout(() => { this.canShowAd = true }, 1000 * 60 * 3)
     },
     initMobileStyle: function() {
         if (window.orientation !== undefined || window.innerHeight <= 722) {
             // это мобильный браузер 
-            if(document.querySelector('#mstyle') == null){
-                console.log(1)
+            if (document.querySelector('#mstyle') == null) {
                 document.querySelector('head').innerHTML += `<link id='mstyle' rel="stylesheet" href="./TemplateData/m.style.css">`
             }
-        }
-        else{
-            console.log(2)
-            if(document.querySelector('#mstyle') != null){
-                console.log(3)
+        } else {
+            if (document.querySelector('#mstyle') != null) {
                 document.querySelector('#mstyle').remove()
             }
         }
     }
 }
 
-let bgAudioPlayer = undefined
-let gameTask = {
+let backgroundAudioSource = undefined
+let GameManager = {
     currentRecord: 0,
     categoryIndex: 0,
+    tryAdWatchRestart: false,
     questionsList: undefined,
     isRun: false,
     canSetAnswer: false,
@@ -104,13 +104,13 @@ let gameTask = {
         this.timerContainer.innerHTML = ''
         this.currentQuestion = question
         let right = question.answers[question.right]
-        //unsort answers
+            //unsort answers
         this.currentQuestion.answers = shuffle(this.currentQuestion.answers)
-        //console.log('right - ' + question.right)
-        for(let i = 0; i < question.answers.length; i++){
-            if(right == this.currentQuestion.answers[i]){
+            //console.log('right - ' + question.right)
+        for (let i = 0; i < question.answers.length; i++) {
+            if (right == this.currentQuestion.answers[i]) {
                 this.currentQuestion.right = i
-                //console.log(i)
+                    //console.log(i)
             }
         }
         //console.log('n_right - ' + this.currentQuestion.right)
@@ -126,7 +126,7 @@ let gameTask = {
         this.canSetAnswer = false
         answer_showAnimateButtons(this.currentQuestion.answers, 1, () => {
             this.canSetAnswer = true
-            gameTimer.play(projectData.game_data.questionTime, (time) => {
+            gameTimer.play(config.projectData.questionTime, (time) => {
                 //console.log('timeout')
                 this.lose()
             })
@@ -144,20 +144,20 @@ let gameTask = {
         this.currentQuestion = undefined
             //console.log('right')
         this.currentRecord += 1
-        //console.log(this.currentRecord)
+            //console.log(this.currentRecord)
             // next question
         if (this.currentRecord < this.questionsList.length) {
             this.frames.setState('right')
-            customSimpleTimer.play(projectData.game_data.questionStepTime, '#next_question_timer', () => {
+            SimpleTimer.play(config.projectData.questionStepTime, '#next_question_timer', () => {
                 this.frames.setState('game')
                 this.questionSet(this.questionsList[this.currentRecord])
             })
         }
         // game won
         else {
-            let isNew = record.set(this.categoryIndex , this.currentRecord)
-            //console.log( quiz[this.categoryIndex].category)
-            if(gameTimer.isRun) gameTimer.stop()
+            let isNew = record.set(this.categoryIndex, this.currentRecord)
+                //console.log( quiz[this.categoryIndex].category)
+            if (gameTimer.isRun) gameTimer.stop()
             document.querySelector('#category_win_text').innerHTML = quiz[this.categoryIndex].category
             this.frames.setState('win')
             this.isRun = false
@@ -166,8 +166,8 @@ let gameTask = {
     lose: function() {
         document.querySelector('#lose_record_text').innerHTML = `${this.currentRecord}/${this.questionsList.length}`
         document.querySelector('#lose_record_pesent').innerHTML = `${Math.round((this.currentRecord/this.questionsList.length)*100)}%`
-        let isNew = record.set(this.categoryIndex , this.currentRecord)
-        //console.log(isNew)
+        let isNew = record.set(this.categoryIndex, this.currentRecord)
+            //console.log(isNew)
         this.currentQuestion = undefined
             //console.log('false')
         this.frames.setState('lose')
@@ -175,7 +175,7 @@ let gameTask = {
         this.isRun = false;
         categoryIndex = NaN
     },
-    StartGame: function(category) {
+    InitQuiz: function(category) {
         if (this.isRun) return
         this.currentRecord = 0;
         this.isRun = true
@@ -190,139 +190,105 @@ let gameTask = {
     }
 }
 let gameTimer = {
-    isRun: false,
-    play: function(time, finishCallback) {
-        if (this.isRun) return
-        this.isRun = true
-        this.timeChangeEvent(0)
-        let currTime = 0
-        let iterId = setInterval(() => {
-            if (!this.isRun) {
-                clearInterval(iterId)
-            } else {
-                if (currTime > time - 1) {
-                    this.isRun = false
-                    finishCallback(time)
+        isRun: false,
+        play: function(time, finishCallback) {
+            if (this.isRun) return
+            this.isRun = true
+            this.timeChangeEvent(0)
+            let currTime = 0
+            let iterId = setInterval(() => {
+                if (!this.isRun) {
                     clearInterval(iterId)
-                } else this.timeChangeEvent(currTime)
-            }
-            currTime++;
-        }, 1000)
-    },
-    stop: function() {
-        this.isRun = false
-    },
-    timeChangeEvent: function(time) {
-        gameTask.timerContainer.innerHTML = 'Осталось ' + (projectData.game_data.questionTime - time - 1) + 'c'
+                } else {
+                    if (currTime > time - 1) {
+                        this.isRun = false
+                        finishCallback(time)
+                        clearInterval(iterId)
+                    } else this.timeChangeEvent(currTime)
+                }
+                currTime++;
+            }, 1000)
+        },
+        stop: function() {
+            this.isRun = false
+        },
+        timeChangeEvent: function(time) {
+            GameManager.timerContainer.innerHTML = 'Осталось ' + (config.projectData.questionTime - time - 1) + 'c'
+        }
     }
-}
-
-let customSimpleTimer = {
-    play: function(time, containerSelector, callback) {
-        let container = document.querySelector(containerSelector)
-        container.innerHTML = time
-        setTimeout(() => {
-            time -= 1
-            if (time > 0) {
-                this.action(time, container)
-                this.play(time, containerSelector, callback)
-            } else {
-                callback()
-            }
-        }, 1000)
-    },
-    action: function(value, container) {
-        container.innerHTML = value
-    }
-}
-
-
-// game start logic
+    // TODO: make locolize
 function init() {
-    console.log(`
-🅼🅰🅳🅴 🅸🅽
-
-█▀█ █░█ █ ▀█   █▀▀ █▄░█ █▀▀ █ █▄░█ █▀▀
-▀▀█ █▄█ █ █▄   ██▄ █░▀█ █▄█ █ █░▀█ ██▄
-    `)
-
+    // events
     window.onblur = onBlurGame
     window.onfocus = onFocusGame
-        // load project data
-    document.addEventListener('click', () => {
-        //if (bgAudioPlayer != undefined) {
-        //    bgAudioPlayer.play()
-        //}
-    })
 
 
-    gameTask.frames.setState('game')
-
-    document.getElementById('gameTitle').innerHTML = localize(projectData.game_data.Title)
-    document.getElementById('gameTitle').style.color = projectData.colors.fontColor
-    document.querySelector('.background').style.filter = 'blur(' + projectData.game_data.bgBlurSize + 'px)'
-    document.querySelector('#choseFrame').style.backgroundColor = projectData.colors.choseFrameColor
-    document.querySelector('#recordFrame').style.backgroundColor = projectData.colors.recordFrameColor
-
-
-    gameTask.isRun = false
-    gameTask.answerContainer = document.querySelector('#answersList')
-    gameTask.questionText = document.querySelector('#questionContainer')
-    gameTask.timerContainer = document.querySelector('#gametimer')
-    gameTask.questtionImage = document.querySelector('#qestionImg')
-
-
+    // Menu elements and logic
+    document.getElementById('gameTitle').innerHTML = localize(config.projectData.Title)
+    document.getElementById('gameTitle').style.color = config.colorData.fontColor
+    document.querySelector('.background').style.filter = 'blur(' + config.projectData.bgBlurSize + 'px)'
+    document.querySelector('#choseFrame').style.backgroundColor = config.colorData.choseFrameColor
+    document.querySelector('#recordFrame').style.backgroundColor = config.colorData.recordFrameColor
     document.querySelector('#menu_play').addEventListener('click', () => {
         if (quiz.length > 1) showHide('#menuFrame', '#choseFrame')
-        else {
-            clickCategoryButton(0)
-        }
+        else clickCategoryButton(0)
+    })
+    document.querySelector('#menu_record').addEventListener('click', () => {
+        showHide('#menuFrame', '#recordFrame')
+        recordDraw()
     })
     document.querySelector('#chose_backmenu').addEventListener('click', () => { showHide('#choseFrame', '#menuFrame') })
     document.querySelector('#record_backmenu').addEventListener('click', () => { showHide('#recordFrame', '#menuFrame') })
-    document.querySelector('#menu_record').addEventListener('click', () => { showHide('#menuFrame', '#recordFrame'); recordDraw() })
     document.querySelector('#settings_backmenu').addEventListener('click', () => { showHide('#settingsFrame', '#menuFrame') })
 
-    if (!projectData.game_data.useLocalization && !projectData.game_data.isPlayMusick) {
+
+    // Game frames logic init
+    GameManager.frames.setState('game')
+    GameManager.isRun = false
+    GameManager.answerContainer = document.querySelector('#answersList')
+    GameManager.questionText = document.querySelector('#questionContainer')
+    GameManager.timerContainer = document.querySelector('#gametimer')
+    GameManager.questtionImage = document.querySelector('#qestionImg')
+
+    // settings frame
+    if (!config.projectData.useLocalization && !config.projectData.isPlayMusick) {
         document.getElementById('menu_settings').style.display = 'none'
     }
     document.querySelector('#menu_settings').addEventListener('click', () => { showHide('#menuFrame', '#settingsFrame') })
+
 
     // category add buttons
     categoryButtonContainer = document.getElementById('categorybutton')
     drawCategory()
 
-
-    //set all block style
-    let alltext = document.getElementsByClassName('text')
-    for (let i = 0; i < alltext.length; i++)
-        alltext[i].style.color = projectData.colors.fontColor
-    let buttons = document.getElementsByTagName('button')
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].style.backgroundColor = projectData.colors.buttonsColor
-        buttons[i].style.color = projectData.colors.fontColor
-    }
-    if (projectData.game_data.isPlayMusick) {
+    // set audio
+    if (config.projectData.isPlayMusick) {
         let slider = document.querySelector('#volume')
-        slider.value = save.get('VOLUME' , 50)
-        volumeSize = slider.value/100
-        //bgAudioPlayer = new Audio('./ProjectData/bgAudio.mp3')
-        //bgAudioPlayer.volume = parseFloat(slider.value/100)
-        //bgAudioPlayer.onended = function() { bgAudioPlayer.play() }
-        // web audio api
-        setTimeout(()=>{
-            loadSound('./ProjectData/bgAudio.mp3')
-            setVolume(slider.value/100)
-        },3500)
-
-        slider.addEventListener('input' , (value)=>{
+        slider.value = save.get('VOLUME', 50)
+        volumeSize = slider.value / 100
+            // start bg audio with delay
+        setTimeout(() => {
+                loadSound('./ProjectData/bgAudio.mp3')
+                setVolume(slider.value / 100)
+            }, 3500)
+            // changed slider volume value
+        slider.addEventListener('input', (value) => {
             value = slider.value
-            save.set('VOLUME' , value)
-            setVolume(value/100)
-            //bgAudioPlayer.volume = parseFloat(value/100)
+            save.set('VOLUME', value)
+            setVolume(value / 100)
         })
     }
-    //bgAudioPlayer.autoplay = true
+
+
+    //set all block style (auto set all block style)
+    let alltext = document.getElementsByClassName('text')
+    for (let i = 0; i < alltext.length; i++)
+        alltext[i].style.color = config.colorData.fontColor
+    let buttons = document.getElementsByTagName('button')
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.backgroundColor = config.colorData.buttonsColor
+        buttons[i].style.color = config.colorData.fontColor
+    }
 }
 
 function drawCategory() {
@@ -339,11 +305,10 @@ function drawCategoryButton(index) {
 }
 
 function clickCategoryButton(index) {
-    if(quiz.length == 1)showHide('#menuFrame', '#gameFrame')
-    else showHide('#choseFrame' , '#gameFrame')
-    gameTask.questionsList = quiz[index]
-    console.log(index)
-    gameTask.StartGame(index)
+    if (quiz.length == 1) showHide('#menuFrame', '#gameFrame')
+    else showHide('#choseFrame', '#gameFrame')
+    GameManager.questionsList = quiz[index]
+    GameManager.InitQuiz(index)
 }
 
 function onBlurGame(event) {
@@ -353,7 +318,7 @@ function onBlurGame(event) {
 
 function onFocusGame(event) {
     MuteAudio(false)
-    //if (bgAudioPlayer != undefined) bgAudioPlayer.play()
+        //if (bgAudioPlayer != undefined) bgAudioPlayer.play()
 }
 
 function showHide(hide, show, classData = 'frame') {
@@ -364,21 +329,22 @@ function showHide(hide, show, classData = 'frame') {
 function recordDraw() {
     let container = document.querySelector('#recordContainer')
     container.innerHTML = ''
-    let a = 0 , b = 0
-    for(let i = 0; i< quiz.length; i++){
-        container.innerHTML += `<div class="recordObj" style="color:${projectData.colors.fontColor};">
+    let a = 0,
+        b = 0
+    for (let i = 0; i < quiz.length; i++) {
+        container.innerHTML += `<div class="recordObj" style="color:${config.colorData.fontColor};">
         <div class="Name text">${quiz[i].category}</div>
         <div class="RecordValue text">${record.get(i)}/${quiz[i].questions.length}</div>
     </div>`
-        a+= quiz[i].questions.length
-        b+= record.get(i)
+        a += quiz[i].questions.length
+        b += record.get(i)
     }
-    let result = Math.round(100*b/a)
+    let result = Math.round(100 * b / a)
     document.querySelector('#game_r_per').innerHTML = result
 }
 
 function answer_showAnimateButtons(array, delay, finich = null) {
-    gameTask.answerContainer.innerHTML = ''
+    GameManager.answerContainer.innerHTML = ''
     for (let i = 0; i < array.length; i++) {
         setTimeout(() => {
             drawAnswerButton(array[i], i)
@@ -389,37 +355,38 @@ function answer_showAnimateButtons(array, delay, finich = null) {
 }
 
 function drawAnswerButton(item, i) {
-    gameTask.answerContainer.innerHTML += `
-    <button id="answer-${i}" class="buttonAnimation" style='color:${projectData.colors.fontColor};background-color:${projectData.colors.buttonsColor};' onclick="clickAnswer(${i})">${item}</button>
+    GameManager.answerContainer.innerHTML += `
+    <button id="answer-${i}" class="buttonAnimation" style='color:${config.colorData.fontColor};background-color:${config.colorData.buttonsColor};' onclick="clickAnswer(${i})">${item}</button>
     `
 }
 
 function clickAnswer(index) {
-    if (gameTask.canSetAnswer) gameTask.answerListener(index)
+    if (GameManager.canSetAnswer) GameManager.answerListener(index)
 }
 
 function goToMenu() {
-    addManager.showInterstitial(function(){})
-    addManager.review()
-    gameTask.frames.setState('game')
+    YaSDKManager.showInterstitial(function() {})
+    YaSDKManager.review()
+    GameManager.frames.setState('game')
     showHide("#gameFrame", "#menuFrame")
 }
 
 function restartGame() {
-    addManager.showInterstitial(()=>{
-        if (gameTask.categoryIndex != NaN || gameTask.categoryIndex != undefined) gameTask.StartGame(gameTask.categoryIndex)
+    YaSDKManager.showInterstitial(() => {
+        if (GameManager.categoryIndex != NaN || GameManager.categoryIndex != undefined) GameManager.InitQuiz(GameManager.categoryIndex)
     })
 }
 
-function shuffle(array) {
-    return array.sort(x => Math.random() - 0.5);
-}
 
-
-window.addEventListener("DOMContentLoaded", (event) => { 
-    addManager.initMobileStyle()
-    init() 
+// game start logic
+window.addEventListener("DOMContentLoaded", (event) => {
+    YaSDKManager.initMobileStyle()
+    init()
+    console.log(`
+    🅼🅰🅳🅴 🅸🅽
+    
+    █▀█ █░█ █ ▀█   █▀▀ █▄░█ █▀▀ █ █▄░█ █▀▀
+    ▀▀█ █▄█ █ █▄   ██▄ █░▀█ █▄█ █ █░▀█ ██▄
+        `)
+    addEventListener('resize', (event) => { YaSDKManager.initMobileStyle() })
 });
-addEventListener('resize' , (event)=>{
-    addManager.initMobileStyle()
-})
